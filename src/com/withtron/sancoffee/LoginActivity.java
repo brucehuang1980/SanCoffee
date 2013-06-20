74af8b0472f3d9dbe42b071c6a34b39b88ef7d33
brucehuang <brucehuang1980@gmail.com>
2013-06-21 00:39:29 +0800

    update log in page icons

--- src/com/withtron/sancoffee/LoginActivity.java
+++ src/com/withtron/sancoffee/LoginActivity.java
@@ -11,9 +11,13 @@ import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 
 public class LoginActivity extends Activity{
@@ -24,7 +28,24 @@ public class LoginActivity extends Activity{
 		
         if(((SanCoffeeApp) getApplication()).getFirstRun()){
     		setContentView(R.layout.activity_login);
-            final Button fb_button = (Button) findViewById(R.id.facebooklogin);
+            
+    		RelativeLayout rl = (RelativeLayout)findViewById(R.id.login_layout);
+            DisplayMetrics dm = new DisplayMetrics();   
+            getWindowManager().getDefaultDisplay().getMetrics(dm); //先取得螢幕解析度  
+            int screenWidth = dm.widthPixels;   //取得螢幕的寬
+            int screenHeight = dm.heightPixels;
+            int buttonWidth = 268*screenWidth/400;
+            int buttonHeight = 46*screenHeight/800;
+            int title_bar_height = 76*screenHeight/800;
+            
+            Button fb_button = new Button(this);
+            // 400*800, 66,522, w:268 h:46
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(buttonWidth, buttonHeight);
+            params.leftMargin = 66*screenWidth/400; // 66/400
+            params.topMargin = 522*screenHeight/800 - title_bar_height; // 522/800
+            fb_button.setLayoutParams(params);
+            //fb_button.setText(getString(R.string.facebook_login));
+            fb_button.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_fb_login));
             fb_button.setOnClickListener(new View.OnClickListener() {
             	public void onClick(View view) {
             		// start Facebook Login
@@ -42,8 +63,16 @@ public class LoginActivity extends Activity{
             	    });
             	}
             });
+            rl.addView(fb_button);
             
-            final Button button_sanlogin = (Button) findViewById(R.id.sanlogin);
+
+            Button button_sanlogin = new Button(this);
+            RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(buttonWidth, buttonHeight);
+            params1.leftMargin = 66*screenWidth/400; 
+            params1.topMargin = 590*screenHeight/800 - title_bar_height;
+            button_sanlogin.setLayoutParams(params1);
+            //fb_button.setText(getString(R.string.san_login));
+            button_sanlogin.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_san_login));
             button_sanlogin.setOnClickListener(new View.OnClickListener() {
             	public void onClick(View view) { 
             		AlertDialog ad = new AlertDialog.Builder(LoginActivity.this).create();
@@ -57,8 +86,15 @@ public class LoginActivity extends Activity{
             		ad.show();
             	}
             });
+            rl.addView(button_sanlogin);
             
-            final Button button_signup = (Button) findViewById(R.id.signup);
+            Button button_signup = new Button(this);
+            RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(buttonWidth, buttonHeight);
+            params2.leftMargin = 66*screenWidth/400; 
+            params2.topMargin = 654*screenHeight/800 - title_bar_height;
+            button_signup.setLayoutParams(params2);
+            //fb_button.setText(getString(R.string.sign_up));
+            button_signup.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_san_signup));
             button_signup.setOnClickListener(new View.OnClickListener() {
             	public void onClick(View view) {                     
             		AlertDialog ad = new AlertDialog.Builder(LoginActivity.this).create();
@@ -72,14 +108,22 @@ public class LoginActivity extends Activity{
             		ad.show();
             	}
             });
+            rl.addView(button_signup);
             
-            final Button button_dontlogin = (Button) findViewById(R.id.dontlogin);
+            Button button_dontlogin = new Button(this);
+            RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(buttonWidth, buttonHeight);
+            params3.leftMargin = 66*screenWidth/400; 
+            params3.topMargin = 720*screenHeight/800 - title_bar_height;
+            button_dontlogin.setLayoutParams(params3);
+            button_dontlogin.setBackgroundDrawable(getResources().getDrawable(R.drawable.button_do_not_login));
+            //fb_button.setText(getString(R.string.dont_login));
             button_dontlogin.setOnClickListener(new View.OnClickListener() {
             	public void onClick(View view) {
                     Intent intent = new Intent(LoginActivity.this, FragmentTabsActivity.class);
                     startActivity(intent);	        		
             	}
             });
+            rl.addView(button_dontlogin);
             
 		}
         else{
